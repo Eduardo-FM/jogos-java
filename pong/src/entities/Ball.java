ec4fdf3c7f9e23fb51224e4b6ad81ce3a53625f4
Eduardo-FM <eduardo.freitas.machado@gmail.com>
2026-05-05 21:27:09 -0300

    ball bouncing

--- pong/src/entities/Ball.java
+++ pong/src/entities/Ball.java
@@ -1,39 +1,66 @@
 package entities;
 
 import config.Config;
-import engine.InputHandler;
-
 import java.awt.*;
 
 public class Ball {
 
-    private Double positionX, positionY;
-    private Double angle = calAngle();
+    private double positionX, positionY;
+    private double angle;
 
 
-    public Ball(Double positionX, Double positionY) {
+    public Ball(double positionX, double positionY) {
         this.positionX = positionX;
         this.positionY = positionY;
+        this.angle = calAngle();
     }
 
     public void update(int screenWidth, int screenHeight) {
         positionX += Config.getBallSpeed() * Math.cos(angle);
-        positionY += Config.getBallSpeed() * Math.sin(angle);;
-        clampPosition(screenWidth, screenHeight);
+        positionY += Config.getBallSpeed() * Math.sin(angle);
+        ballReflection(screenWidth, screenHeight);
     }
 
     public void render(Graphics g) {
         g.setColor(Color.WHITE);
-        g.fillRect(positionX.intValue(), positionY.intValue(), Config.getBallWidth(), Config.getBallHeight());
+        g.fillRect( (int) positionX, (int) positionY,
+                Config.getBallWidth(), Config.getBallHeight());
     }
 
-    private void clampPosition(int screenWidth, int screenHeight){
-        positionX = Math.max(0, Math.min(positionX, screenWidth - Config.getBallWidth()));
-        positionY = Math.max(0, Math.min(positionY, screenHeight - Config.getBallHeight()));
+    private void ballReflection(int screenWidth, int screenHeight){
+        int ballWidth = Config.getBallWidth();
+        int ballHeight = Config.getBallHeight();
+
+        if (positionX <= 0) {
+            positionX = 0.0;
+            angle = Math.PI - angle;
+        }
+
+        if (positionX + ballWidth >= screenWidth) {
+            positionX = (double) (screenWidth - ballWidth);
+            angle = Math.PI - angle;
+        }
+
+        if (positionY <= 0) {
+            positionY = (double) 0;
+            angle = -angle;
+        }
+
+        if (positionY + ballHeight >= screenHeight) {
+            positionY = (double) (screenHeight - ballHeight);
+            angle = -angle;
+        }
     }
 
     private static Double calAngle() {
         double angle = Math.random() * (2 * Math.PI);
+
+        double minAngle = Math.toRadians(20);
+
+        if (Math.abs(Math.cos(angle)) < Math.cos(Math.PI / 2 - minAngle)) {
+            angle += minAngle;
+        }
+
         return angle;
     }
 }
