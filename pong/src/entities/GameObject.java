51475d7355f88953e8c02327169e0c8967fb502d
Eduardo-FM <eduardo.freitas.machado@gmail.com>
2026-05-14 20:11:37 -0300

    starting GameObject

--- pong/src/entities/GameObject.java
+++ pong/src/entities/GameObject.java
@@ -4,53 +4,5 @@ import config.Config;
 
 public abstract class GameObject {
 
-    private double positionX, positionY;
-    private double speed;
-    private int width;
-    private int height;
-
-    public double getPositionX() {
-        return positionX;
-    }
-
-    public void setPositionX(double positionX) {
-        this.positionX = positionX;
-    }
-
-    public double getPositionY() {
-        return positionY;
-    }
-
-    public void setPositionY(double positionY) {
-        this.positionY = positionY;
-    }
-
-    public void setPosition(double positionX , double positionY) {
-        this.positionX = positionX;
-        this.positionY = positionY;
-    }
-
-    public double getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(double speed) {
-        this.speed = speed;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
+    private double positionX;
 }
